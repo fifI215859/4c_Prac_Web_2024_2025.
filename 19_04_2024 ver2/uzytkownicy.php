12daf9637dce7e71a4988a22e0f884be07ad804d
fifI215859 <112875113+fifI215859@users.noreply.github.com>
2024-04-19 13:16:19 +0200

    Add files via upload

---
+++ 19_04_2024 ver2/uzytkownicy.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+<!DOCTYPE html>
+    <html lang="Pl">
+<meta charset="UTF-8">
+    <link rel="stylesheet" href="style5.css">
+<head>
+    <title>Portal społecznościowy</title>
+</head>
+    <header>
+        <div>
+            <p><h2>Logowanie</h2></p>
+                <div>
+                        <div id="blokPrawy">
+                                <p><h2>Wizytówka</h2></p>
+                        </div id="blokPrawy">
+                </div>
+        </div>  
+    </header>
+        <body>
+            <img src="o3.jpg">
+        </body>
+<footer>
+    <div class="stopka">Strone opracował: Filip Bugaj<div class="stopka">
+</footer>
+</html>
